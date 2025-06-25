24480e640fdf331a77947edd5e16a4ac262262b6
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2025-06-25 20:02:28 +0100

    mi first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'verde-800': '#065036',
+        'green-900': '#2b4e3f',
+        'bg': '#f3f1ef',
+        'bg-25': '#fefaf7',
+        'naranjado-50': '#e9b18a',
+        'new': '#ec623e',
+        'negro': '#1a1a1a',
+        'dark-nature': '#7c3a24',
+        'rosa': '#f0dddb',
+      },
+      fontFamily: {
+        sans: ['Cerebri Sans', 'sans-serif'],
+        serif: ['Cooper', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
